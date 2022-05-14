744fc891e904a22a31d350ec4d6261a4d3db8b7e
Arhat161 <Arhat161@users.noreply.github.com>
2022-05-14 15:27:49 +0300

    firs commit

--- src/main/java/ru/netology/javacore/TodoServer.java
+++ src/main/java/ru/netology/javacore/TodoServer.java
@@ -5,17 +5,48 @@ import com.google.gson.Gson;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class TodoServer {
-    //...
 
-    public TodoServer(int port, Todos todos) {
-        //...
+    static class Response {
+        String type;
+        String task;
+
+        @Override
+        public String toString() {
+            return "type = " + type + ", task = " + task;
+        }
     }
 
-    public void start() throws IOException {
+    private int port = 8081;
+
+    public TodoServer(int port, Todos todos) {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("New connection accepted");
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                String json = in.readLine();
+                Response r = new Gson().fromJson(json, Response.class);
+                switch (r.type) {
+                    case "ADD":
+                        todos.addTask(r.task);
+                        break;
+                    case "REMOVE":
+                        todos.removeTask(r.task);
+                        break;
+                }
+                out.println(todos.getAllTasks());
+                System.out.println(r);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void start() {
         System.out.println("Starting server at " + port + "...");
-        //...
     }
 }
--- src/test/java/ru/netology/javacore/TodosTests.java
+++ src/test/java/ru/netology/javacore/TodosTests.java
@@ -1,6 +1,104 @@
 package ru.netology.javacore;
 
+import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class TodosTests {
 
-    // ваши тесты для класса Todos
+    @BeforeAll
+    public static void initSuite() {
+        System.out.println("Running 'TodosTests' for methods of class \"Todos\"");
+    }
+
+    @AfterAll
+    public static void completeSuite() {
+        System.out.println("All 'TodosTests' complete! ");
+    }
+
+    @BeforeEach
+    public void initTest() {
+        System.out.println("Starting new test");
+    }
+
+    @AfterEach
+    public void finalizeTest() {
+        System.out.println("Test complete!");
+    }
+
+    @Test
+    @DisplayName("Test \"addTask()\" method")
+    public void addTaskTest(TestInfo addTaskTestInfo) {
+
+        Todos todo = new Todos();
+
+        todo.addTask("Doing something!");
+
+        boolean actual = todo.getAllTasks().contains("Doing something!");
+
+        Assertions.assertTrue(actual, addTaskTestInfo.getDisplayName() + " NO complete!");
+        System.out.println(addTaskTestInfo.getDisplayName() + " complete!");
+    }
+
+    @Test
+    @DisplayName("Test \"removeTask()\" method")
+    public void removeTaskTest(TestInfo removeTaskTestInfo) {
+
+        Todos todo = new Todos();
+
+        todo.addTask("Doing something!");
+        todo.removeTask("Doing something!");
+
+        boolean expected = false;
+        boolean actual = todo.getAllTasks().contains("Doing something!");
+
+        Assertions.assertEquals(expected, actual, removeTaskTestInfo.getDisplayName() + " NO complete!");
+        System.out.println(removeTaskTestInfo.getDisplayName() + " complete!");
+    }
+
+
+    @Test
+    @DisplayName("Test \"getAllTask()\" method")
+    public void getAllTasksTest(TestInfo getAllTasksTestInfo) {
+
+        Todos todo = new Todos();
+
+        StringBuilder exp = new StringBuilder();
+
+        exp.append("First task");
+        exp.append(" ");
+        exp.append("Second task");
+        exp.append(" ");
+        exp.append("Third task");
+        exp.append(" ");
+
+        String expected = exp.toString();
+
+        todo.addTask("First task");
+        todo.addTask("Second task");
+        todo.addTask("Third task");
+
+        Assertions.assertEquals(expected, todo.getAllTasks(), getAllTasksTestInfo.getDisplayName() + " NO complete!");
+        System.out.println(getAllTasksTestInfo.getDisplayName() + " complete!");
+    }
+
+    @Test
+    @DisplayName("Test \"getAllTasksAsList()\" method")
+    public void getAllTasksAsListTest(TestInfo getAllTasksAsListTestInfo) {
+        Todos todo = new Todos();
+
+        todo.addTask("First task");
+        todo.addTask("Second task");
+        todo.addTask("Third task");
+
+        ArrayList<String> expected = new ArrayList<>(Arrays.asList("First task", "Second task", "Third task"));
+
+        ArrayList<String> actual = todo.getAllTasksAsList();
+
+        Assertions.assertEquals(expected, actual, getAllTasksAsListTestInfo.getDisplayName() + " NO complete!");
+        System.out.println(getAllTasksAsListTestInfo.getDisplayName() + " complete!");
+    }
+
+
 }
